a3cba95fd02e88066fe612951a50b814bd685ca5
Ribamar FS <ribafs@gmail.com>
2020-10-02 12:34:59 -0300

    Adjusts

--- acl/PermissionsServiceProvider.php
+++ acl/PermissionsServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Providers;
 
-use App\Models\Permission;
+use App\Permission;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
