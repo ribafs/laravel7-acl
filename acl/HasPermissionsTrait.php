44f3627189189eb8cf17f781b9b23ea8a7c6b9a7
Ribamar FS <ribafs@gmail.com>
2020-10-03 08:24:50 -0300

    Add

--- acl/HasPermissionsTrait.php
+++ acl/HasPermissionsTrait.php
@@ -16,8 +16,10 @@ trait HasPermissionsTrait {
 
    }
 
-    // Parâmetros: role. Ex: $user->hasRole('admin')
-    // Checar se o user atual detem a role especificada
+    // ROLES
+
+    // Parâmetro: roles. Ex: $user->hasRole('admin', 'super')
+    // Checar se o user atual detem uma das roles especificadas
     // Retorno: true/false
    public function hasRole( ... $roles ) {
        foreach ($roles as $role) {
@@ -27,6 +29,44 @@ trait HasPermissionsTrait {
        }
        return false;
     }
+
+    protected function getAllRoles(array $roles) {
+        return Role::whereIn('slug',$roles)->get();    
+    }
+
+    // Criar uma role a ser gravada em 'roles'
+    // Parâmetros: $name e $slug da role a ser criada
+    // Sem retorno, grava e mostra na tela a role criada
+    public function createRole($name, $slug){
+        $role = Role::create([
+            'name' => $name,
+            'slug' => $slug
+        ]);
+        return $role;
+    }
+
+    // Parâmetro: roles. Ex: $user->giveRolesTo('editor','author')// as roles já devem estar em 'roles' e serão atribuidas ao user locado
+    // Atribuir roles para o user atual, gravadas na tabela user_role
+    // Sem retorno. Grava as roles na tabela user_role para o user atual
+    public function giveRolesTo(... $roles) {
+       $roles = $this->getAllRoles($roles);
+       if($roles === null) {
+          return $this;
+       }
+       $this->roles()->saveMany($roles);
+       return $this;
+    }
+
+    // Remove uma ou mais roles do user atual, que estão em user_role
+    // Parãmetros: roles. Ex: $user->deleteRoles('admin', 'user')
+    // Sem retorno. Grava as informações na tabela e mostra na tela dados do suer atual
+    public function deleteRoles( ... $roles ) {
+       $roles = $this->getAllRoles($roles);
+       $this->roles()->detach($roles);
+       return $this;
+    }
+
+    // PERMISSIONS
 
     protected function hasPermission($permission) {
        return (bool) $this->permissions->where('slug', $permission)->count();
@@ -46,22 +86,31 @@ trait HasPermissionsTrait {
 
     // Parâmetro: permission. $user->hasPermissionTo('clients-index'). As permissões devem estar em 'permisions'
     // Checar se o user atual detem a permission citada
-    // $user->hasPermissionTo('clients-index'). As permissões devem estar em 'permisions' e a função retorna true ou false
     // Retorno true/false
     public function hasPermissionTo($permission) {
        return (bool) $this->hasPermissionThroughRole($permission) || $this->hasPermission($permission);
+    }
+
+    // Criar uma permission a ser gravada em 'permissions'
+    // Parâmetros: $name e $slug da permission a ser criada
+    // Sem retorno, grava e mostra na tela a permission criada
+    public function createPermission($name, $slug){
+        $permission = Permission::create([
+            'name' => $name,
+            'slug' => $slug
+        ]);
+        return $permission;
     }
 
     protected function getAllPermissions(array $permissions) {
         return Permission::whereIn('slug',$permissions)->get();    
     }
 
-    // Parâmetros: permissions. Ex: $user->givePermissionsTo('clients-index','clients-edit')// as permissões já devem estar em 'permissions'
+    // Parâmetro: $permissions. Ex: $user->givePermissionsTo('clients-index','clients-edit')// as permissões já devem estar em 'permissions'
     // Grava permissões para o user atual, na tabela user_permission
     // Sem retorno. Grava as permissões na tabela user_permission para o user atual
     public function givePermissionsTo(... $permissions) {
        $permissions = $this->getAllPermissions($permissions);
-//       dd($permissions);
        if($permissions === null) {
           return $this;
        }
@@ -77,5 +126,4 @@ trait HasPermissionsTrait {
        $this->permissions()->detach($permissions);
        return $this;
     }
-
 }
