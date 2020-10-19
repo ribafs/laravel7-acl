e8462e2799e74690dd99c42300181a37edcdd3a6
Ribamar FS <ribafs@gmail.com>
2020-10-19 18:54:03 -0300

    Up

--- up/HomeController.php
+++ up/HomeController.php
@@ -1,26 +1,16 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('welcome');
@@ -28,7 +18,19 @@ class HomeController extends Controller
 
     public function home()
     {
-        return view('home');
-    }
+        $super = Auth::user()->hasRole('super');
+        $admin = Auth::user()->hasRole('admin');
+        $manager = Auth::user()->hasRole('manager');
+        $user = Auth::user()->hasRole('user');
 
+        if($super || $admin) {
+            return redirect('/admin/users');
+        }elseif($manager || $user) {
+            return redirect('/admin/clients');
+        }else{
+            return view('home');
+        }
+    }
 }
+
+
--- up/web.php
+++ up/web.php
@@ -1,7 +1,4 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
 Auth::routes();
 
 Route::get('/', 'HomeController@index');
